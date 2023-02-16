bb9b5d45ee8f34c1b78a2ed20241c01dda9b3e5a
SamWarden <SamWardenSad@gmail.com>
2023-02-16 03:10:11 +0300

    Add the project structure

---
+++ src/application/user/commands/create-user.ts
@@ -0,0 +1,22 @@
+import { UserRepo } from "@src/application/user/interfaces/persistence"
+import { UUID } from "@src/domain/common/types"
+import { User } from "@src/domain/user/entities"
+import { TonAddress } from "@src/domain/user/types"
+
+export class CreateUser {
+  constructor(
+    readonly id: UUID,
+    readonly address: TonAddress,
+  ) {}
+}
+
+export class CreateUserHandler {
+  constructor(
+    readonly userRepo: UserRepo,
+  ) {}
+
+  async execute(command: CreateUser): Promise<void> {
+    const user = User.create(command.id, command.address)
+    await this.userRepo.addUser(user)
+  }
+}
---
+++ src/domain/user/entities.ts
@@ -0,0 +1,15 @@
+import { UUID } from "@src/domain/common/types"
+import { TonAddress } from "./types"
+
+
+export class User {
+  constructor(
+    readonly id: UUID,
+    readonly address: TonAddress,
+    readonly registeredAt: Date
+  ) {}
+
+  static create(id: UUID, address: TonAddress): User {
+    return new User(id, address, new Date())
+  }
+}
