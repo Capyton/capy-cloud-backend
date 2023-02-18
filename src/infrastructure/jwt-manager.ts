2a44a64acf45b527fb366af2161615de3b14bb5b
SamWarden <SamWardenSad@gmail.com>
2023-02-19 01:39:13 +0300

    Add implementation for JwtManager

---
+++ src/infrastructure/jwt-manager.ts
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken"
+import { JwtManager } from "@src/application/auth/interfaces"
+import { User } from "@src/domain/user/entities"
+
+export class JwtManagerImpl implements JwtManager {
+  constructor(
+    private readonly secret: string,
+    private readonly  expiresIn: number = 3600,  // default value is 1 hour
+  ) {}
+
+  generateToken(user: User): string {
+    const payload = { id: user.id, address: user.address };
+    return jwt.sign(payload, this.secret, { algorithm: "HS256", expiresIn: this.expiresIn });
+  }
+}
