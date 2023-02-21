7442d733575670bfe41e5b45be7aab8c70ac6c82
Desiders <47452083+Desiders@users.noreply.github.com>
2023-02-21 19:08:40 +0300

    Add `UserRepo` in repositories/

---
+++ src/infrastructure/db/repositories/user.ts
@@ -0,0 +1,21 @@
+import { UserAddressNotFound } from "@src/application/user/exceptions";
+import { User } from "@src/domain/user/entities";
+import { TonAddress } from "@src/domain/user/types";
+import { QueryRunner } from "typeorm";
+
+export class UserRepo {
+    constructor(private readonly queryRunner: QueryRunner) { }
+
+    async getUserByAddress(address: TonAddress): Promise<User> {
+        const user = await this.queryRunner.manager.findOne(User, { where: { address } })
+        if (!user) {
+            throw new UserAddressNotFound(`User with address ${address} not found`)
+        }
+
+        return user
+    }
+
+    async addUser(user: User): Promise<void> {
+        await this.queryRunner.manager.insert(User, user)
+    }
+}
