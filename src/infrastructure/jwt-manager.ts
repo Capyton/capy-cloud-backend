e951e8c3001a30447c0f2f80255cd468469e5a75
Desiders <47452083+Desiders@users.noreply.github.com>
2023-02-24 21:14:49 +0300

    Rename attribute from `secret` to `privateKey`

--- src/infrastructure/jwt-manager.ts
+++ src/infrastructure/jwt-manager.ts
@@ -1,15 +1,15 @@
-import jwt from "jsonwebtoken"
 import { JwtManager } from "@src/application/auth/interfaces"
 import { User } from "@src/domain/user/entities"
+import jwt from "jsonwebtoken"
 
 export class JwtManagerImpl implements JwtManager {
   constructor(
-    private readonly secret: string,
+    private readonly privateKey: string,
     private readonly expiresIn: number = 3600,  // default value is 1 hour
-  ) {}
+  ) { }
 
   generateToken(user: User): string {
     const payload = { id: user.id, address: user.address }
-    return jwt.sign(payload, this.secret, { algorithm: "HS256", expiresIn: this.expiresIn })
+    return jwt.sign(payload, this.privateKey, { algorithm: "HS256", expiresIn: this.expiresIn })
   }
 }
