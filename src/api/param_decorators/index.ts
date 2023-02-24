168eba2a61c069e074002b9046efa8fa65b976d8
Desiders <47452083+Desiders@users.noreply.github.com>
2023-02-24 19:38:50 +0300

    Add re-exports in param_decorators/ for repositories/

--- src/api/param_decorators/index.ts
+++ src/api/param_decorators/index.ts
@@ -1,1 +1,9 @@
 export { UnitOfWork } from "./uow"
+export {
+    AuthUserRepo,
+    FileReader, FileRepo,
+    ProviderReader, ProviderRepo,
+    ProviderBagReader, ProviderBagRepo,
+    UserReader, UserRepo,
+    UserBagReader, UserBagRepo
+} from "./repositories"
