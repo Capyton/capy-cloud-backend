6586e7dbd5eb24ebf800cd180503d00ff629f889
Desiders <47452083+Desiders@users.noreply.github.com>
2023-03-06 01:31:46 +0300

    Fix bug and improve structure

--- global_configs/init.sh
+++ global_configs/init.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 set -e
 
-mkdir -p storage-daemon/private
-
 if [ "$#" -ne 1 ]; then
     echo "Exact 1 argument required: config path or mainnet/testnet"
     exit 1
 fi
 
 if [ "$1" == "testnet" ]; then
-    wget -q -O storage-daemon/private/testnet.json https://ton.org/testnet-global.config.json
+    wget -q -O global_configs/testnet.json https://ton.org/testnet-global.config.json
     echo "Downloading testnet global config file"
 elif [ "$1" == "mainnet" ]; then
-    wget -q -O storage-daemon/private/mainnet.json https://ton.org/global.config.json
+    wget -q -O global_configs/mainnet.json https://ton.org/global.config.json
     echo "Downloading mainnet global config file"
 fi
