e6eb8b064d0d2a23d38ec7b32c60311911966796
SamWarden <SamWardenSad@gmail.com>
2023-03-25 20:39:43 +0300

    Add `Bearer` check to the `UserPayloadFromAuthToken`

--- src/api/param_decorators/auth-token.ts
+++ src/api/param_decorators/auth-token.ts
@@ -5,11 +5,23 @@ import { ExecutionContext, UnauthorizedException, createParamDecorator } from "@
 import { JwtManager } from "@src/application/auth/interfaces"
 import { Request } from "express"
 
+
+function checkAuthData(authData: string): string | null {
+    const regex = /^Bearer:\s*(?<authToken>\S+)\s*$/
+    const match = authData.match(regex)
+    if (match && match.groups) {
+        return match.groups.authToken || null
+    }
+    return null
+}
+
+
 export const UserPayloadFromAuthToken = createParamDecorator(
     (_data: unknown, ctx: ExecutionContext) => {
         const request: Request = ctx.switchToHttp().getRequest()
 
-        const authToken = request.headers.authorization
+        const authToken = checkAuthData(request.headers.authorization || "") || ""
+
         // Get the `JwtManager` from the request, which was set in a jwt manager middleware
         const jwtManager: JwtManager = request.app.locals.jwtManager
 
